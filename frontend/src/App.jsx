1bfa9bcbbdd1b33782465e068309e1ec81417565
Sangeet Kumar <135730503+Sangeet96@users.noreply.github.com>
2024-11-19 10:28:19 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   const handlePredict = async () => {
     if (validateForm()) {
       try {
-        const response = await fetch('http://localhost:3000/predict', {
+        const response = await fetch('https://sonar-rock-or-mine-predictor-backend.vercel.app/predict', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
